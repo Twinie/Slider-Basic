47e162c12dc9d7a9ffb34a8ee1eddf28d8ae2ff6
Twinkle <twinklegohilchauhan@outlook.com>
2022-02-09 15:49:40 +0530

    added apex chart

--- app.js
+++ app.js
@@ -17,18 +17,37 @@
     });
   
     var options = {
-        chart: {
-          type: 'line'
-        },
-        series: [{
-          name: 'sales',
-          data: [30,40,35,50,49,60,70,91,125]
-        }],
-        xaxis: {
-          categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
-        }
+      series: [{
+        name: "Desktops",
+        data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+    }],
+      chart: {
+      height: 350,
+      type: 'line',
+      zoom: {
+        enabled: true
       }
-      
-      var chart = new ApexCharts($("#chart"), options);
-      
-      chart.render();
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: 'straight'
+    },
+    title: {
+      text: 'Product Trends by Month',
+      align: 'left'
+    },
+    grid: {
+      row: {
+        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5
+      },
+    },
+    xaxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+    }
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
